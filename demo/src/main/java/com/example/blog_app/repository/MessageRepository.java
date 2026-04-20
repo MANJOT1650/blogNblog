bab7ac04fb754cfe542a53236bdbca1e2161836d
Manjot04 <manjotmanjot2004@gmail.com>
2026-04-20 12:34:16 +0530

    update

--- demo/src/main/java/com/example/blog_app/repository/MessageRepository.java
+++ demo/src/main/java/com/example/blog_app/repository/MessageRepository.java
@@ -1,7 +1,6 @@
 package com.example.blog_app.repository;
 
 import com.example.blog_app.model.Message;
-import com.example.blog_app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
